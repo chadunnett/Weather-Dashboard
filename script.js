b485073b26c6b7747cbe45c8dedb1b0d3388514b
chadunnett <chadunnett@gmail.com>
2022-10-04 18:04:20 -0500

    queryurl

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// API-key 9dbe76981e248581151f53806ee2f190
+var APIKey = 9dbe76981e248581151f53806ee2f190;
+var city;
+// url = api.openweathermap.org/data/2.5/weather?q={city}&appid={APIkey}
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
